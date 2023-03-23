9737e5fc1f9fa45c55f4e82c5bc87a84dc16e6d6
ismahaneyy <liciousmayrah@gmail.com>
2023-03-23 08:24:03 +0300

    completed assignment

--- app/models/bird.rb
+++ app/models/bird.rb
@@ -1,6 +1,3 @@
 class Bird < ApplicationRecord
-    def create
-        bird = Bird.create(name: params[:name], species: params[:species])
-        render json: bird, status: :created
-    end
+   
 end
